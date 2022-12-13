9b1665697edd16bea7124ed8fd11fd9324fe4f72
Dulce-Abigail <datxi9806@gmail.com>
2022-12-12 23:43:27 -0600

    Pantalla General Mejorada

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { InicioSecionComponent } from "./components/inicio-secion/inicio-secion.component";
 import { RegistroComponent } from "./components/registro/registro.component";
+import { VistaGeneralComponent } from "./components/vista-general/vista-general.component";
 
 const routes: Routes = [
   {path:'', redirectTo: '/inicioSecion', pathMatch: 'full'},
   {path:'inicioSecion', component: InicioSecionComponent},
   {path: 'registrarse', component: RegistroComponent},
-
+  {path: 'pantallageneral', component: VistaGeneralComponent},
 ];
 
 @NgModule({
